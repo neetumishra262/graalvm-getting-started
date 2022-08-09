e62de1ebbc8e95543bf94fdde8b49335ff4698e9
Neetu Tripathi <neetu.tripathi@dell.com>
2022-08-09 17:20:46 +0530

    Initial commit

--- src/test/java/org/acme/getting/started/ReactiveGreetingResourceIT.java
+++ src/test/java/org/acme/getting/started/ReactiveGreetingResourceIT.java
@@ -3,7 +3,7 @@ package org.acme.getting.started;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+public class ReactiveGreetingResourceIT extends ReactiveGreetingResourceTest {
 
     // Execute the same tests but in native mode.
 }
